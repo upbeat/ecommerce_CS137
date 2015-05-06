582bb17d7f447a3faef02d4cc264b1976f420e3d
josephgit45 <josephlee.45@gmail.com>
2015-05-05 21:35:32 -0700

    Added active map to contact info

--- contact_info.php
+++ contact_info.php
@@ -26,7 +26,8 @@
       </div>
 
       <div id="map_image">
-        <img src="img/mapLocation.png" alt="Eternal peace" height="275" width="390">
+        <a href="https://www.google.com/maps/place/Ayala+Science+Library+(UCI)/@33.645829,-117.846848,17z/data=!3m2!4b1!5s0x80dcde116fb34a9d:0xd0f9d0e6912f7783!4m2!3m1!1s0x80dcde116e368795:0x171bf8af6caae1da"><img border = "0" src ="img/mapLocation.png"></a>
+        <p>Click on map to open in Google Maps</p>
       </div>
     </div>
 
